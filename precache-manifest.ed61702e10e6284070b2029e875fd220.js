ddd6949a60e5dd1c048c7fe29a5a6583a07d0c41
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-05-07 19:59:19 +0530

    Updates

---
+++ precache-manifest.ed61702e10e6284070b2029e875fd220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6e7281d8bf1ded682e7a96c9e3547c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43120699f9e7d251a209",
+    "url": "/monsters-rolodex/static/css/main.4ee834d0.chunk.css"
+  },
+  {
+    "revision": "9d0e70860a082811e223",
+    "url": "/monsters-rolodex/static/js/2.c391ae1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c391ae1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43120699f9e7d251a209",
+    "url": "/monsters-rolodex/static/js/main.6b317e93.chunk.js"
+  },
+  {
+    "revision": "593bb1bf5a5cc4071efe",
+    "url": "/monsters-rolodex/static/js/runtime-main.750416da.js"
+  }
+]);
